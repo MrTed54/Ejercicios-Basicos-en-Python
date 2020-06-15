1af03dd688b3593dd49a414303ac84626f058987
unknown <fabian54mrted@gmail.com>
2020-06-15 01:35:53 -0500

    Tipos_de_Datos

---
+++ tipos_de_datos.py
@@ -0,0 +1,26 @@
+#Tipos de Datos
+#
+#integer
+a = 10
+
+#String
+cadea = "Hola";
+
+#flotante
+pi = 3.14;
+
+#Cadena larga
+nombre = "Tu nombre es pato";
+
+#Boolean
+bandera = True;
+
+#Dato Nulo;
+null = None;
+
+print (a);
+print(pi)
+print(nombre)
+print(bandera)
+print(null)
+print (cadea)
